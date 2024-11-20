5f27a364559aa46a04a36581af5e2dc91c3cf87b
empedangiunhatptit <phamngoclinhtq2006@gmail.com>
2024-11-20 21:51:37 +0700

    Add files via upload

---
+++ Session02.Ex05.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	int chieu dai =20,chieu rong =10;
+	int S=cd*cr,P=(cd+cr)*2;
+	printf("chu vi hinh chu nhat la %d \n",P);
+	printf("dien tich hinh chu nhat la %d \n",S);
+	return 0;
+}
